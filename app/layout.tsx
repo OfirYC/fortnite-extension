815414f3b0be55be4c01db4a2188fc91925bd06a
Djboy08 <justindoane@gmail.com>
2023-07-13 05:35:54 -0400

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Dreamday Car Rental",
@@ -21,8 +18,8 @@ export default function RootLayout({
         content="width=device-width, initial-scale=1, minimum-scale=1"
       ></meta>
 
-      <body className={inter.className + " bg-custom-bg overflow-x-hidden"}>
-        <div className=" overflow-x-hidden overflow-y-visible">{children}</div>
+      <body className={" bg-custom-bg overflow-x-hidden"}>
+        <div className="overflow-x-hidden overflow-y-visible ">{children}</div>
         {/* <script src=""></script> */}
       </body>
     </html>
